19a38c8248a605e3f7cd3bf0fd1040e8fa4aa739
himanshu-5s339 <143061462+himanshu-5s339@users.noreply.github.com>
2024-04-08 14:08:01 +0530

    first commit

---
+++ pages/product/[slug].js
@@ -0,0 +1,228 @@
+import Wrapper from "@/components/Wrapper";
+import { IoMdHeartEmpty } from "react-icons/io";
+import React, { useState } from "react";
+import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
+import ReletedProducts from "@/components/ReletedProducts";
+import { fetchDataFromApi } from "@/Utils/api";
+import { getDiscountedPricePercentage } from "@/Utils/helper";
+import ReactMarkdown from "react-markdown";
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "@/store/cartSlice";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ProductDetails = ({ product, products }) => {
+  const [selectedSize, setSelectedSize] = useState();
+  const [showError, setShowError] = useState(false);
+  const dispatch = useDispatch();
+  const p = product?.data?.[0]?.attributes;
+
+  const notify = () => {
+    toast.success("Success. Check your cart!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  };
+  return (
+    <div className="w-full md:py-20">
+      <ToastContainer />
+      <Wrapper>
+        {/* left column start */}
+        <div className="flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]">
+          <div className="w-full md:w-auto flex-[1.5] max-w-[500px] lg:max-w-full mx-auto lg:mx-0">
+            <ProductDetailsCarousel images={p.image.data} />
+          </div>
+          {/* left column end */}
+
+          {/* right column start */}
+          <div className="flex-[1] py-3">
+            {/* Product title */}
+            <div className="text-[34px] font-semibold mb-2 leading-tight">
+              {p.name}
+            </div>
+
+            {/* Product subtitle */}
+            <div className="text-lg font-semibold mb-5">{p.subtitle}</div>
+
+            {/* Products price */}
+            <div className="flex items-center">
+              <p className="mr-2 text-lg font-semibold">
+                MRP : &#8377;{p.price}
+              </p>
+              {p.original_price && (
+                <>
+                  <p className="text-base  font-medium line-through">
+                    &#8377;{p.original_price}
+                  </p>
+                  <p className="ml-auto text-base font-medium text-green-500">
+                    {getDiscountedPricePercentage(p.original_price, p.price)}%
+                    off
+                  </p>
+                </>
+              )}
+            </div>
+
+            <div className="text-md font-medium text-black/[0.5]">
+              incl. of taxes
+            </div>
+            <div className="text-md font-medium text-black/[0.5] mb-20">
+              {`(Also includes all application duties)`}
+            </div>
+            {/* product size range start */}
+            <div className="mb-10">
+              {/* headind start */}
+              <div className="flex justify-between mb-2">
+                <div className="text-md font-semibold">Select Size</div>
+                <div className="text-md font-medium text-black/[0.5] cursor-pointer">
+                  Select Guide
+                </div>
+              </div>
+              {/* heading end */}
+
+              {/* size start */}
+              <div id="sizesGrid" className="grid grid-cols-3 gap-2">
+                {p.size.data.map((item, i) => (
+                  <div
+                    key={i}
+                    className={`border rounded-md text-center py-3 font-medium ${
+                      item.enabled
+                        ? "hover:border-black cursor-pointer"
+                        : "cursor-not-allowed bg-black/[0.1] opacity-50"
+                    } ${selectedSize === item.size ? "border-black" : ""}`}
+                    onClick={() => {
+                      setSelectedSize(item.size);
+                      setShowError(false);
+                    }}
+                  >
+                    {item.size}
+                  </div>
+                ))}
+                {/* <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6
+                </div> */}
+              </div>
+              {/* size end */}
+
+              {/* SHOW ERROR START */}
+
+              {showError && (
+                <div className="text-red-600 mt-1">
+                  Size selection is required
+                </div>
+              )}
+
+              {/* SHOW ERROR END */}
+            </div>
+            {/* product size range end */}
+            {/* button */}
+            <button
+              className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75"
+              onClick={() => {
+                if (!selectedSize) {
+                  setShowError(true);
+                  document.getElementById("sizesGrid").scrollIntoView({
+                    block: "center",
+                    behavior: "smooth",
+                  });
+                } else {
+                  dispatch(
+                    addToCart({
+                      ...product?.data?.[0],
+                      selectedSize,
+                      oneQuantityPrice: p.price,
+                    })
+                  );
+                  notify();
+                }
+              }}
+            >
+              Add to Cart
+            </button>
+
+            {/* /button */}
+            {/* WHISHLIST BUTTON START */}
+            <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
+              Whishlist
+              <IoMdHeartEmpty size={20} />
+            </button>
+            {/* WHISHLIST BUTTON END */}
+            <div>
+              {/* <div className="text-lg font-bold mb-5">Product Details</div> */}
+              <div className="markdown text-md mb-5">
+                <ReactMarkdown>{p.description}</ReactMarkdown>
+              </div>
+            </div>
+          </div>
+          {/* right column end */}
+        </div>
+        <ReletedProducts products={products} />
+      </Wrapper>
+    </div>
+  );
+};
+
+export default ProductDetails;
+
+export async function getStaticPaths() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+  const paths = products?.data?.map((p) => ({
+    params: {
+      slug: p.attributes.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const product = await fetchDataFromApi(
+    `/api/products?populate=*&filters[slug][$eq]=${slug}`
+  );
+  const products = await fetchDataFromApi(
+    `/api/products?populate=*&[filters][slug][$ne]=${slug}`
+  );
+
+  return {
+    props: {
+      product,
+      products,
+    },
+  };
+}
