60d35baac2219a63ff0c47babfdb8d878a4bb080
ramkrishna455 <148609989+ramkrishna455@users.noreply.github.com>
2024-04-11 11:21:42 +0530

    Update urls.js

--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,4 @@
 export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
-export const API_URL =
+export const NEXT_PUBLIC_API_URLS =
   process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:1337";
