bdd384a2c28d395cee8a672747bf357057aafb15
ramkrishna455 <148609989+ramkrishna455@users.noreply.github.com>
2024-04-11 11:22:17 +0530

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-import { API_URL, STRAPI_API_TOKEN } from "./urls";
+import { NEXT_PUBLIC_API_URLS, STRAPI_API_TOKEN } from "./urls";
 
 export const fetchDataFromApi = async (endpoint) => {
   const options = {
@@ -8,14 +8,14 @@ export const fetchDataFromApi = async (endpoint) => {
     },
   };
 
-  const res = await fetch(`${API_URL}${endpoint}`, options);
+  const res = await fetch(`${NEXT_PUBLIC_API_URLS}${endpoint}`, options);
   const data = await res.json();
 
   return data;
 };
 
 export const makePaymentRequest = async (endpoint, payload) => {
-  const res = await fetch(`${API_URL}${endpoint}`, {
+  const res = await fetch(`${NEXT_PUBLIC_API_URLS}${endpoint}`, {
     method: "POST",
     headers: {
       Authorization: "Bearer " + STRAPI_API_TOKEN,
